69e36c18348c9eeaa2d181c2c9bfad827b84dc22
Damien Cartron <Damien.Cartron@cnrs.fr>
2025-06-23 14:13:38 +0200

    ça fait un moment que je n'ai pas fait un git et j'ai fait des petites modifs partout

--- 3.ImportFichiersMM.R
+++ 3.ImportFichiersMM.R
@@ -14,6 +14,7 @@ library(dplyr)
 library(openxlsx)
 library(tidyverse)
 library(here)
+library(arrow)
 MM25 <- here()
 MMin <- here("D:/Document/Cours&TD/QESS 2024-25/MonMaster/Candidatures/MM/MM_in")
 MMout <- here("D:/Document/Cours&TD/QESS 2024-25/MonMaster/Candidatures/MM/MM_out")
@@ -28,12 +29,23 @@ Eval <- read_parquet("emm.parquet")
 Eval <- Eval |>  rename(NumOrdre = NumAlpha,  ClassTot = NoteDC, Classement = Rang, NoteEval = fusion, NoteE1 = R1Note, NoteE2 = R2Note, Eval1 = R1, Eval2= R2)
 EvalComplete <- Eval 
 Eval <- Eval  |> 
-  select(NumOrdre, id, ClassTot, NoteEval,  Classement, NoteE1, NoteE2, Commentaires, Eval1, Eval2)
+  select(NumOrdre, id, Cursus, ClassTot, NoteEval,  Classement, NoteE1, NoteE2, Commentaires, Eval1, Eval2)
   # select(c('NumOrdre', 'id', 'ClassTot', 'NoteEval',  'Classement', 'NoteE1', 'NoteE2', 'Commentaires', 'Eval1' , 'Eval2'))
 
 ## Base Candidat : le fichier monmaster avant évaluation -----
 
 setwd(MMin)
+
+# petit bloc pour modifier le nom des fichiers qui a été changé en cours de route !----- 
+listdossiers<- list.files(recursive = TRUE, full.names = FALSE) |> 
+  as_tibble() |> 
+  filter(str_starts(value, "Candidatures_EHESS PARIS-1900125")) |> 
+  mutate(
+    correctname = str_replace(value, "Candidatures_EHESS PARIS-1900125", "Candidatures-EHESS PARIS-1900125")
+  )
+file.rename(from = listdossiers$value, to = listdossiers$correctname)
+
+# lecture base candidat ---- 
 BaseCandidat <- read_excel("20250401_08H56_0753742K_EHESS PARIS_330_org.xlsx") |> 
                   rename(id = `Numéro de candidat`,
                          Nom = 'Nom de naissance',
@@ -41,9 +53,12 @@ BaseCandidat <- read_excel("20250401_08H56_0753742K_EHESS PARIS_330_org.xlsx") |
                          mail = 'Adresse e-mail',
                          Civilite = 'Civilité',
                          DOB = 'Date de naissance', 
-                          MentionBac = 'Mention obtenue')
-
-BaseCandidat <- BaseCandidat %>% select(id, Nom, Prenom, mail, Civilite, DOB, MentionBac)
+                         MentionBac = 'Mention obtenue') |> 
+  mutate(
+    Prenom = str_to_title(Prenom, locale = "fr"), 
+    Nom = str_to_upper(Nom, locale = "fr"), 
+    CandidatP = str_glue("{Nom} {Prenom}")
+  ) |> select(id, Nom, Prenom, CandidatP, mail, Civilite, DOB, MentionBac)
 
 setwd(MMin)
 
@@ -51,12 +66,16 @@ setwd(MMin)
 importdate <- function(date){
   assign(paste0("ENS_",gsub("-", "", date)), value = read_excel(paste0("Candidatures-EHESS PARIS-1900125MCBW2_",gsub("-", "", date),".xlsx")),envir = .GlobalEnv)
   assign(paste0("EHESS_",gsub("-", "", date)), value = read_excel(paste0("Candidatures-EHESS PARIS-1900125M6PVU_",gsub("-", "", date),".xlsx")),envir = .GlobalEnv)
+  # assign(paste0("PDI-ENS_",gsub("-", "", date)), value = read_excel(paste0("Candidatures-EHESS PARIS-1900125MM1FR_",gsub("-", "", date),".xlsx")),envir = .GlobalEnv)
+  # assign(paste0("PDI-EHESS_",gsub("-", "", date)), value = read_excel(paste0("Candidatures-EHESS PARIS-1900125MHFU8_",gsub("-", "", date),".xlsx")),envir = .GlobalEnv)
 
 
   #fichiers du jour 
     assign("ENS", value = read_excel(paste0("Candidatures-EHESS PARIS-1900125MCBW2_",gsub("-", "", date),".xlsx")),envir = .GlobalEnv)
     assign("EHESS", value = read_excel(paste0("Candidatures-EHESS PARIS-1900125M6PVU_",gsub("-", "", date),".xlsx")),envir = .GlobalEnv) # l'autre mode d'assignation global eest <<- 
-
+    assign("PDI_EHESS", value = read_excel(paste0("Candidatures-EHESS PARIS-1900125MHFU8_",gsub("-", "", Sys.Date()),".xlsx")),envir = .GlobalEnv)
+    assign("PDI_ENS", value = read_excel(paste0("Candidatures-EHESS PARIS-1900125MM1FR_",gsub("-", "", Sys.Date()),".xlsx")),envir = .GlobalEnv)
+    
   
   #supprimer dans EHESS les candidats avec statut== non_classee
     EHESS <- EHESS[EHESS$Statut != "NON_CLASSEE",]
@@ -68,12 +87,34 @@ importdate <- function(date){
                                    dem_EHESS = Démissionnaire, 
                                    R_EHESS = Classement, 
                                    LC_EHESS = `Position sur liste d’attente`, 
-                                   id = `Identifiant candidat`)
+                                   id = `Identifiant candidat`) |> 
+    select(-Candidat) |> 
+    left_join(BaseCandidat |> select(id, CandidatP), by = "id") |> 
+    rename(Candidat = CandidatP)
+  
   ENS <- ENS %>% dplyr::rename(Statut_ENS = Statut, 
                                dem_ENS = Démissionnaire, 
                                R_ENS = Classement, 
                                LC_ENS = `Position sur liste d’attente`,
-                               id = `Identifiant candidat`)
+                               id = `Identifiant candidat`)|> 
+    select(-Candidat) |> 
+    left_join(BaseCandidat |> select(id, CandidatP), by = "id") |> 
+    rename(Candidat = CandidatP)
+  
+  PDI_EHESS <- PDI_EHESS  |> 
+    rename(Statut_EHESS = Statut, 
+            dem_EHESS = Démissionnaire, 
+            R_EHESS = Classement, 
+            LC_EHESS = `Position sur liste d’attente`, 
+            id = `Identifiant candidat`)
+  
+  PDI_ENS <- PDI_ENS  |> 
+    rename(Statut_ENS = Statut, 
+            dem_ENS = Démissionnaire, 
+            R_ENS = Classement, 
+            LC_ENS = `Position sur liste d’attente`,
+            id = `Identifiant candidat`)
+  
   
   # suppression de colonnes inutiles 
   EHESS <<- EHESS %>% select(-`Motif de non classement / refus`)
@@ -86,6 +127,29 @@ importdate <- function(date){
   tot <- merge(tot,BaseCandidat, by = "id", all = FALSE)
   
   tot$Candidat <- ifelse(is.na(tot$Candidat.x), tot$Candidat.y, tot$Candidat.x)
+  
+  PDI <<- PDI_EHESS |> 
+    select(id, Statut_EHESS, LC_EHESS) |> 
+    left_join(PDI_ENS |> select(id, Statut_ENS, LC_ENS), by = "id") |> 
+    mutate(
+      Resultat_PDI =  case_when(
+        Statut_EHESS == "ACCEPTEE_DEFINITIVEMENT"     | Statut_ENS == "ACCEPTEE_DEFINITIVEMENT"      ~"Admis_Def",
+        Statut_EHESS == "ACCEPTEE_NON_DEFINITIVEMENT" | Statut_ENS == "ACCEPTEE_NON_DEFINITIVEMENT"  ~"Admis_Prov",
+        Statut_EHESS == "EN_ATTENTE_DE_REPONSE"       | Statut_ENS == "EN_ATTENTE_DE_REPONSE"        ~"AttenteRep",
+        Statut_EHESS == "LISTE_ATTENTE"               | Statut_ENS == "LISTE_ATTENTE"                ~"LC",
+        Statut_EHESS == "REFUSEE"                     | Statut_ENS == "REFUSEE"                      ~"Refusé",
+        Statut_EHESS == "NON_CLASSEE"                 | Statut_ENS == "NON_CLASSEE"                  ~"NC",
+        .default = "Non Traité"), 
+      
+      Etb_PDI = case_when(
+        Statut_EHESS == "ACCEPTEE_DEFINITIVEMENT" | Statut_EHESS == "ACCEPTEE_NON_DEFINITIVEMENT" ~"EHESS",
+        Statut_ENS == "ACCEPTEE_DEFINITIVEMENT" | Statut_ENS == "ACCEPTEE_NON_DEFINITIVEMENT" ~"ENS"), 
+    ) |> 
+    relocate(Statut_ENS, .after = Statut_EHESS) |> 
+    rename(Statut_EHESS_PDI = Statut_EHESS, 
+           Statut_ENS_PDI = Statut_ENS, 
+           LC_EHESS_PDI = LC_EHESS, 
+           LC_ENS_PDI = LC_ENS)
   
   # suppression des colonnes inutiles
   tot <- tot %>% select(-c(Candidat.x, Candidat.y))
@@ -107,6 +171,25 @@ importdate <- function(date){
             Statut_EHESS == "LISTE_ATTENTE"               | Statut_ENS == "LISTE_ATTENTE"               ~"LC",
             Statut_EHESS == "REFUSEE"                     | Statut_ENS == "REFUSEE"                     ~"Refusé",
             .default = "Non Traité"), 
+        
+        St_EHESS = case_when(
+          Statut_EHESS == "ACCEPTEE_DEFINITIVEMENT"     ~"Admis_Def",
+          Statut_EHESS == "ACCEPTEE_NON_DEFINITIVEMENT" ~"Admis_Prov",
+          Statut_EHESS == "EN_ATTENTE_DE_REPONSE"       ~ "AttenteRep",
+          Statut_EHESS == "LISTE_ATTENTE"               ~"LC",
+          Statut_EHESS == "REFUSEE"                     ~"Refusé",
+          is.na(Statut_EHESS) ~ NA_character_,
+          .default = "Non Traité"), 
+        
+        St_ENS = case_when(
+          Statut_ENS == "ACCEPTEE_DEFINITIVEMENT"      ~"Admis_Def",
+          Statut_ENS == "ACCEPTEE_NON_DEFINITIVEMENT"  ~"Admis_Prov",
+          Statut_ENS == "EN_ATTENTE_DE_REPONSE"        ~ "AttenteRep",
+          Statut_ENS == "LISTE_ATTENTE"                ~"LC",
+          Statut_ENS == "REFUSEE"                      ~"Refusé",
+          is.na(Statut_ENS) ~ NA_character_,
+          .default = "Non Traité"), 
+      
             
         Etb = case_when(
               Statut_EHESS == "ACCEPTEE_DEFINITIVEMENT" | Statut_EHESS == "ACCEPTEE_NON_DEFINITIVEMENT" ~"EHESS",
@@ -124,7 +207,8 @@ importdate <- function(date){
           MentionBac == "Sans mention" ~"sans"), 
         MentionBac = fct_relevel(as_factor(MentionBac), "sans", "AB", "Bien", "TB", "TTB")
             ) |> 
-      relocate(id,Genre, Candidat, Etb, Resultat, Statut_EHESS, Statut_ENS, R_EHESS, R_ENS, LC_EHESS, LC_ENS, MentionBac) |> 
+      select(-c(`Adresse électronique.x`,`Adresse électronique.y`)) |> 
+      relocate(id,Genre, Candidat, Etb, Resultat, St_EHESS, St_ENS, Statut_EHESS, Statut_ENS, R_EHESS, R_ENS, LC_EHESS, LC_ENS, MentionBac) |> 
       arrange(Classement)
 
   # tot$Etb[tot$Statut_EHESS == "ACCEPTEE_DEFINITIVEMENT" | tot$Statut_EHESS == "ACCEPTEE_NON_DEFINITIVEMENT"] <- "EHESS" 
@@ -182,9 +266,18 @@ df_list <- mget(ls(pattern = "^Synthese_")) # Crée une liste (très particuliè
 Synthese_20250603 <- Synthese_20250603 |> mutate(R_EHESS = as.character(R_EHESS), R_ENS = as.character(R_ENS))
 PileData <- bind_rows(df_list, .id = "source") # le merge qui marche avec une variable  source qui porte le nom du fichier 
 PileData$DateFichier <- as.Date(gsub("Synthese_","",PileData$source), format = "%Y%m%d") # je crée ma variable DateFichier qui est bien une date avec un format date
-
+# names(PileData)
 PileData <- PileData |> 
-  left_join(EvalComplete |> select(id, PDI), by = "id")
+  left_join(EvalComplete |> select(id, PDI), by = "id") |> 
+  left_join(PDI |> select(id, Resultat_PDI, Etb_PDI, LC_EHESS_PDI, LC_ENS_PDI), by = "id") |> 
+    mutate(
+    Synt_PDI = if_else(PDI != "NC" & (Resultat_PDI == "Admis_Def" | Resultat_PDI == "Admis_Prov" | Resultat_PDI == "AttenteRep") , Resultat_PDI, str_glue("LCEH_{LC_EHESS_PDI}/LCEN_{LC_ENS_PDI}"))
+    # Synt_PDI = if_else(Resultat_PDI %in% c("Admis_Def", "Admis_Prov", "AttenteRep") & Resultat_PDI != "NC" & !is.na(Resultat_PDI), Resultat_PDI, str_glue("LCEH_{LC_EHESS_PDI}/LCEN_{LC_ENS_PDI}"))
+  )
+
+# il faudrait créer synthèse PDI dès la fonctoin pour l'avoir dans les fichier synthses
+# il faudrait faire un version abbrégée de Statut_EHESS et ENS avec une nouvelle variable pour éviter les pbs de changement de noms 
+
 
 
 ## Base Candidat pour Envoi à Lionel Arnoux ENS -----
@@ -203,4 +296,20 @@ PileData <- PileData |>
 # Arnoux[dim(Arnoux)[1],9] <- "ACCEPTEE_DEFINITIVEMENT"
 # 
 # Arnoux %>% filter(Statut == "ACCEPTEE_DEFINITIVEMENT" | Statut == "ACCEPTEE_NON_DEFINITIVEMENT") %>%  write.xlsx(paste0("Envoi_Lionel-Arnoux_", gsub("-", "", Sys.Date()), ".xlsx"), rowNames = FALSE)
+
+#écrire aux étudiants admis prov et def 
+# SyntheseDuJour |>
+#   filter(Resultat == "Admis_Def" | Resultat == "Admis_Prov") |>
+#   select(Candidat, Resultat, Etb, mail) |>
+#   arrange(Candidat) |>
+#   select(mail)
+
+# écrire aux étudiants sur liste d'attente 
+# SyntheseDuJour |>
+#   filter(Resultat == "LC" ) |>
+#   select(Candidat, Resultat, Classement, mail) |>
+#   arrange(Classement) |>
+#   select(mail)
+
+
 
